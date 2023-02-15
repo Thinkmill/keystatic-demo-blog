51ed60ebb9783678e0b5b42ee88a83e28f3e210d
Thomas Whitaker <tom@tomwritescode.com>
2023-02-15 17:27:28 +1100

    Tidying

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,5 @@
 import type { InferGetStaticPropsType } from 'next'
-import PostCard from '../../components/PostCard'
+import dateFormatter from '../../utils/dateFormatter'
 import { createReader } from 'keystatic/reader'
 import config from '../../keystatic'
 
@@ -34,8 +34,18 @@ export default function Blog({
   return (
     <ul className='grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3'>
       {posts.map((post) => (
-        <li className='items-stretch' key={post.slug}>
-          <PostCard post={post} />
+        <li className='items-stretch list-none' key={post.slug}>
+          return (
+          <a href={`blog/${post.slug}`}>
+            {post.coverImage && <img src={post.coverImage} alt='Cover image' />}
+            <p>
+              {dateFormatter(post.publishedDate, 'do MMM yyyy')} |{' '}
+              {post.authors && post.authors.map((author) => author)}
+            </p>
+            <h3>{post.title}</h3>
+            <p>{post.summary}</p>
+          </a>
+          )
         </li>
       ))}
     </ul>
