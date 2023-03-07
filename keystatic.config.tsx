9cd42150df6112402486a94283890991efcae158
Emma Hamilton <git@emmas.town>
2023-03-07 13:13:08 +1000

    Update keystatic

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,9 +1,9 @@
-import { collection, config, fields, singleton } from '@keystatic/core'
-import { ComponentBlocks } from './components/ComponentBlocks'
+import { collection, config, fields, singleton } from "@keystatic/core";
+import { ComponentBlocks } from "./components/ComponentBlocks";
 
 export default config({
   storage: {
-    kind: 'github',
+    kind: "github",
     repo: {
       owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
       name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
@@ -11,8 +11,8 @@ export default config({
   },
   singletons: {
     home: singleton({
-      label: 'Home',
-      directory: 'content/pages/home',
+      label: "Home",
+      path: "content/pages/home/",
       schema: {
         content: fields.document({
           formatting: true,
@@ -24,14 +24,14 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: 'Content',
+          label: "Content",
           componentBlocks: ComponentBlocks,
         }),
       },
     }),
     about: singleton({
-      label: 'About',
-      directory: 'content/pages/about',
+      label: "About",
+      path: "content/pages/about/",
       schema: {
         content: fields.document({
           formatting: true,
@@ -43,7 +43,7 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: 'Content',
+          label: "Content",
           componentBlocks: ComponentBlocks,
         }),
       },
@@ -51,29 +51,29 @@ export default config({
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      directory: 'content/posts',
-      slugField: 'slug',
+      label: "Posts",
+      path: "content/posts/*/",
+      slugField: "slug",
       schema: {
         title: fields.text({
-          label: 'Title',
+          label: "Title",
           validation: { length: { min: 4 } },
         }),
         slug: fields.text({
-          label: 'Slug',
+          label: "Slug",
           validation: { length: { min: 4 } },
         }),
         summary: fields.text({
-          label: 'Summary',
+          label: "Summary",
           validation: { length: { min: 4 } },
         }),
-        publishedDate: fields.date({ label: 'Published Date' }),
-        coverImage: fields.text({ label: 'Image' }),
+        publishedDate: fields.date({ label: "Published Date" }),
+        coverImage: fields.text({ label: "Image" }),
         wordCount: fields.integer({
-          label: 'Word count',
+          label: "Word count",
         }),
-        authors: fields.array(fields.text({ label: 'Name' }), {
-          label: 'Authors',
+        authors: fields.array(fields.text({ label: "Name" }), {
+          label: "Authors",
           itemLabel: (props) => props.value,
         }),
         content: fields.document({
@@ -86,36 +86,36 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: 'Content',
+          label: "Content",
           componentBlocks: ComponentBlocks,
         }),
       },
     }),
     externalArticles: collection({
-      label: 'External Article',
-      directory: 'content/externalArticles',
-      slugField: 'title',
+      label: "External Article",
+      path: "content/externalArticles/*/",
+      slugField: "title",
       schema: {
         title: fields.text({
-          label: 'Title',
+          label: "Title",
           validation: { length: { min: 4 } },
         }),
         directLink: fields.url({
-          label: 'Article Link',
+          label: "Article Link",
         }),
         source: fields.text({
-          label: 'Link Source',
-          defaultValue: 'Read more.',
+          label: "Link Source",
+          defaultValue: "Read more.",
         }),
         coverImage: fields.text({
-          label: 'Cover Image',
+          label: "Cover Image",
         }),
         summary: fields.text({
-          label: 'Summary',
+          label: "Summary",
           validation: { length: { min: 4, max: 200 } },
         }),
-        publishedDate: fields.date({ label: 'Published Date' }),
+        publishedDate: fields.date({ label: "Published Date" }),
       },
     }),
   },
-})
+});
