d28480855a1805bfa61dd9439433e48cc64c696d
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-08 09:51:50 +1300

    added conditional storage based on node env

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,18 +1,30 @@
-import { collection, config, fields, singleton } from "@keystatic/core";
-import { ComponentBlocks } from "./components/ComponentBlocks";
+import {
+  collection,
+  config,
+  fields,
+  GitHubConfig,
+  LocalConfig,
+  singleton,
+} from '@keystatic/core'
+import { ComponentBlocks } from './components/ComponentBlocks'
+
+const storage: LocalConfig['storage'] | GitHubConfig['storage'] =
+  process.env.NODE_ENV === 'development'
+    ? { kind: 'local' }
+    : {
+        kind: 'github',
+        repo: {
+          owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
+          name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+        },
+      }
 
 export default config({
-  storage: {
-    kind: "github",
-    repo: {
-      owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
-      name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
-    },
-  },
+  storage,
   singletons: {
     home: singleton({
-      label: "Home",
-      path: "content/pages/home/",
+      label: 'Home',
+      path: 'content/pages/home/',
       schema: {
         content: fields.document({
           formatting: true,
@@ -24,14 +36,14 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: "Content",
+          label: 'Content',
           componentBlocks: ComponentBlocks,
         }),
       },
     }),
     about: singleton({
-      label: "About",
-      path: "content/pages/about/",
+      label: 'About',
+      path: 'content/pages/about/',
       schema: {
         content: fields.document({
           formatting: true,
@@ -43,7 +55,7 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: "Content",
+          label: 'Content',
           componentBlocks: ComponentBlocks,
         }),
       },
@@ -51,29 +63,29 @@ export default config({
   },
   collections: {
     posts: collection({
-      label: "Posts",
-      path: "content/posts/*/",
-      slugField: "slug",
+      label: 'Posts',
+      path: 'content/posts/*/',
+      slugField: 'slug',
       schema: {
         title: fields.text({
-          label: "Title",
+          label: 'Title',
           validation: { length: { min: 4 } },
         }),
         slug: fields.text({
-          label: "Slug",
+          label: 'Slug',
           validation: { length: { min: 4 } },
         }),
         summary: fields.text({
-          label: "Summary",
+          label: 'Summary',
           validation: { length: { min: 4 } },
         }),
-        publishedDate: fields.date({ label: "Published Date" }),
-        coverImage: fields.text({ label: "Image" }),
+        publishedDate: fields.date({ label: 'Published Date' }),
+        coverImage: fields.text({ label: 'Image' }),
         wordCount: fields.integer({
-          label: "Word count",
+          label: 'Word count',
         }),
-        authors: fields.array(fields.text({ label: "Name" }), {
-          label: "Authors",
+        authors: fields.array(fields.text({ label: 'Name' }), {
+          label: 'Authors',
           itemLabel: (props) => props.value,
         }),
         content: fields.document({
@@ -86,36 +98,36 @@ export default config({
             [2, 1],
             [1, 2, 1],
           ],
-          label: "Content",
+          label: 'Content',
           componentBlocks: ComponentBlocks,
         }),
       },
     }),
     externalArticles: collection({
-      label: "External Article",
-      path: "content/externalArticles/*/",
-      slugField: "title",
+      label: 'External Article',
+      path: 'content/externalArticles/*/',
+      slugField: 'title',
       schema: {
         title: fields.text({
-          label: "Title",
+          label: 'Title',
           validation: { length: { min: 4 } },
         }),
         directLink: fields.url({
-          label: "Article Link",
+          label: 'Article Link',
         }),
         source: fields.text({
-          label: "Link Source",
-          defaultValue: "Read more.",
+          label: 'Link Source',
+          defaultValue: 'Read more.',
         }),
         coverImage: fields.text({
-          label: "Cover Image",
+          label: 'Cover Image',
         }),
         summary: fields.text({
-          label: "Summary",
+          label: 'Summary',
           validation: { length: { min: 4, max: 200 } },
         }),
-        publishedDate: fields.date({ label: "Published Date" }),
+        publishedDate: fields.date({ label: 'Published Date' }),
       },
     }),
   },
-});
+})
