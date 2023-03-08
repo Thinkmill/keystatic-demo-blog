834524905ec5292bdb3fd9b5440d2b0dd4179c09
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-08 15:55:29 +1300

    refactor authors to AvatarList

---
+++ components/AvatarList.tsx
@@ -0,0 +1,29 @@
+import Img from 'next/image'
+
+type Props = {
+  authors: {
+    slug: string | null
+    name?: string | undefined
+    avatar?: string | null | undefined
+  }[][]
+}
+
+const AvatarList = ({ authors }: Props) => {
+  return (
+    <ul className='flex -space-x-4'>
+      {authors.map((author) => (
+        <li key={author.slug}>
+          <Img
+            className='w-12 h-12 rounded-full ring-2 ring-white'
+            alt={`Avatar for ${author.name}`}
+            src={`/images/authors/${author.slug}/${author.avatar}`}
+            width={96}
+            height={96}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default AvatarList
--- pages/[post].tsx
+++ pages/[post].tsx
@@ -1,58 +1,55 @@
-import Img from "next/image";
+import type { InferGetStaticPropsType, GetStaticPropsContext } from 'next'
+import { createReader } from '@keystatic/core/reader'
+import config from '../keystatic.config'
+import dateFormatter from '../utils/dateFormatter'
+import readTime from '../utils/readTime'
+import { DocumentRenderer } from '@keystatic/core/renderer'
 
-import type { InferGetStaticPropsType, GetStaticPropsContext } from "next";
-import { createReader } from "@keystatic/core/reader";
-import config from "../keystatic.config";
-import dateFormatter from "../utils/dateFormatter";
-import readTime from "../utils/readTime";
-import { DocumentRenderer } from "@keystatic/core/renderer";
-
-import Banner from "../components/Banner";
-import InlineCTA from "../components/InlineCTA";
-import Divider from "../components/Divider";
-import YouTubeEmbed from "../components/YouTubeEmbed";
-import TweetEmbed from "../components/TweetEmbed";
-import LoopingVideo from "../components/LoopingVideo";
-import Image from "../components/Image";
-import Testimonial from "../components/Testimonial";
+import Banner from '../components/Banner'
+import InlineCTA from '../components/InlineCTA'
+import Divider from '../components/Divider'
+import YouTubeEmbed from '../components/YouTubeEmbed'
+import TweetEmbed from '../components/TweetEmbed'
+import LoopingVideo from '../components/LoopingVideo'
+import Image from '../components/Image'
+import Testimonial from '../components/Testimonial'
+import AvatarList from '../components/AvatarList'
 
 export async function getStaticPaths() {
-  const reader = createReader("", config);
+  const reader = createReader('', config)
   // Get collection of all posts
-  const postSlugs = await reader.collections.posts.list();
+  const postSlugs = await reader.collections.posts.list()
   return {
     // Generate paths for each post
     paths: postSlugs.map((slug) => ({
       params: { post: slug },
     })),
     fallback: false,
-  };
+  }
 }
 
 export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
-  const slug = params?.post;
+  const slug = params?.post
 
-  if (typeof slug !== "string") {
-    throw new Error("What? WHYYYY");
+  if (typeof slug !== 'string') {
+    throw new Error('What? WHYYYY')
   }
 
-  const reader = createReader("", config);
+  const reader = createReader('', config)
   // Get data for post matching current slug
-  const post = await reader.collections.posts.read(slug);
+  const post = await reader.collections.posts.read(slug)
 
   const authorsData = post
     ? await Promise.all(
         post.authors.map(async (authorSlug) => {
-          const author = await reader.collections.authors.read(
-            authorSlug || ""
-          );
-          return { ...author, slug: authorSlug };
+          const author = await reader.collections.authors.read(authorSlug || '')
+          return { ...author, slug: authorSlug }
         })
       )
-    : [];
+    : []
 
   // Get async data from 'content'
-  const content = await (post?.content() || []);
+  const content = await (post?.content() || [])
   return {
     props: {
       post: {
@@ -61,63 +58,51 @@ export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
       },
       authors: authorsData,
     },
-  };
-};
+  }
+}
 
-export type PostProps = InferGetStaticPropsType<typeof getStaticProps>["post"];
+export type PostProps = InferGetStaticPropsType<typeof getStaticProps>['post']
 export type AuthorProps = InferGetStaticPropsType<
   typeof getStaticProps
->["authors"];
+>['authors']
 
 type TheLot = {
-  post: PostProps;
-  authors: AuthorProps[];
-};
+  post: PostProps
+  authors: AuthorProps[]
+}
 
 export default function Post({ post, authors }: TheLot) {
-  const names = authors.map((author) => author.name);
-  const formattedNames = new Intl.ListFormat("en-AU")
+  const names = authors.map((author) => author.name)
+  const formattedNames = new Intl.ListFormat('en-AU')
     .format(names)
-    .replace("and", "&");
+    .replace('and', '&')
   return (
-    <div className="max-w-4xl mx-auto px-4 md:px-10 prose">
-      <div className="flex gap-4 items-center not-prose">
-        <ul className="flex -space-x-4">
-          {authors.map((author) => (
-            <li key={author.slug}>
-              <Img
-                className="w-12 h-12 rounded-full ring-2 ring-white"
-                alt={`Avatar for ${author.name}`}
-                src={`/images/authors/${author.slug}/${author.avatar}`}
-                width={96}
-                height={96}
-              />
-            </li>
-          ))}
-        </ul>
+    <div className='max-w-4xl mx-auto px-4 md:px-10 prose'>
+      <div className='flex gap-4 items-center not-prose'>
+        <AvatarList authors={authors} />
         {formattedNames}
       </div>
 
-      <div className="flex justify-between mt-4 mb-9">
-        <span className="flex gap-1">
+      <div className='flex justify-between mt-4 mb-9'>
+        <span className='flex gap-1'>
           {post.publishedDate && (
-            <p className="my-0">
-              {dateFormatter(post.publishedDate, "do MMM yyyy")}
+            <p className='my-0'>
+              {dateFormatter(post.publishedDate, 'do MMM yyyy')}
             </p>
           )}
           {post.wordCount && post.wordCount !== 0 ? (
-            <p className="my-0">· {readTime(post.wordCount)}</p>
+            <p className='my-0'>· {readTime(post.wordCount)}</p>
           ) : null}
         </span>
       </div>
       <h1>{post.title}</h1>
-      <p className="text-lg">{post.summary}</p>
+      <p className='text-lg'>{post.summary}</p>
       {post.coverImage && (
-        <div className="not-prose mb-10">
+        <div className='not-prose mb-10'>
           <img
             src={`/${post.coverImage}`}
-            className="w-full rounded-md"
-            alt="Cover image"
+            className='w-full rounded-md'
+            alt='Cover image'
           />
         </div>
       )}
@@ -167,5 +152,5 @@ export default function Post({ post, authors }: TheLot) {
         }}
       />
     </div>
-  );
+  )
 }
