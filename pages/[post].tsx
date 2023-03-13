0edfc70b818cb65356c420019203319a72574623
Simon Vrachliotis <simon@simonswiss.com>
2023-03-13 16:13:01 +1100

    Content blocks (#10)
    * About page content, authors with avatars, relationship fields in place

--- pages/[post].tsx
+++ pages/[post].tsx
@@ -82,7 +82,7 @@ export default function Post({ post, authors }: TheLot) {
     .format(names)
     .replace("and", "&");
   return (
-    <div className="max-w-4xl mx-auto px-4 md:px-10 prose">
+    <div className="max-w-4xl mx-auto px-4 md:px-10">
       <Seo
         title={post.title}
         imagePath={
@@ -91,82 +91,89 @@ export default function Post({ post, authors }: TheLot) {
             : "/images/seo-image.png"
         }
       />
-      <div className="flex gap-4 items-center not-prose">
+      <div className="flex gap-3 items-center flex-wrap">
         {authors && <AvatarList authors={authors} />}
-        {formattedNames}
+        <p className="font-semibold text-gray-900">{formattedNames}</p>
       </div>
 
-      <div className="flex justify-between">
-        <span className="flex gap-1">
+      <div className="mt-4 flex justify-between">
+        <span className="flex gap-1 text-gray-700">
           {post.publishedDate && (
-            <p className="my-0">
+            <p className="">
               {dateFormatter(post.publishedDate, "do MMM yyyy")}
             </p>
           )}
           {post.wordCount && post.wordCount !== 0 ? (
-            <p className="my-0">· {readTime(post.wordCount)}</p>
+            <p className="">· {readTime(post.wordCount)}</p>
           ) : null}
         </span>
       </div>
-      <h1 className="mt-4">{post.title}</h1>
-      <p className="text-lg">{post.summary}</p>
-      {post.coverImage && (
-        <div className="mt-10 not-prose">
-          <NextImage
-            width={1536}
-            height={800}
-            src={`/images/posts/${post.slug}/${post.coverImage}`}
-            alt={`${post.title} Cover image`}
-            className="w-full rounded-md"
+
+      <div className="mt-8 prose max-w-none">
+        <h1 className="mt-4">{post.title}</h1>
+        <p className="text-lg">{post.summary}</p>
+        {post.coverImage && (
+          <div className="mt-10 not-prose">
+            <NextImage
+              width={1536}
+              height={800}
+              src={`/images/posts/${post.slug}/${post.coverImage}`}
+              alt={`${post.title} Cover image`}
+              className="w-full rounded-md"
+            />
+          </div>
+        )}
+        <div className="mt-10">
+          <DocumentRenderer
+            document={post.content}
+            componentBlocks={{
+              inlineCta: (props) => (
+                <InlineCTA
+                  title={props.title}
+                  summary={props.summary}
+                  linkButton={{
+                    externalLink: props.externalLink,
+                    href: props.href,
+                    label: props.linkLabel,
+                  }}
+                />
+              ),
+              divider: (props) => <Divider noIcon={props.noIcon} />,
+              banner: (props) => (
+                <Banner
+                  heading={props.heading}
+                  bodyText={props.bodyText}
+                  externalLink={{
+                    href: props.externalLinkHref,
+                    label: props.externalLinkLabel,
+                  }}
+                />
+              ),
+              youtubeEmbed: (props) => (
+                <YouTubeEmbed youtubeLink={props.youtubeLink} />
+              ),
+              tweetEmbed: (props) => <TweetEmbed tweet={props.tweet} />,
+              loopingVideo: (props) => (
+                <LoopingVideo src={props.src} caption={props.caption} />
+              ),
+              image: (props) => (
+                <Image
+                  src={props.src}
+                  alt={props.alt}
+                  caption={props.caption}
+                />
+              ),
+              testimonial: (props) => (
+                <Testimonial
+                  quote={props.quote}
+                  author={props.author}
+                  workplaceOrSocial={props.workplaceOrSocial}
+                  socialLink={props.socialLink}
+                />
+              ),
+            }}
           />
         </div>
-      )}
-      <div className="mt-10">
-        <DocumentRenderer
-          document={post.content}
-          componentBlocks={{
-            inlineCta: (props) => (
-              <InlineCTA
-                title={props.title}
-                summary={props.summary}
-                linkButton={{
-                  externalLink: props.externalLink,
-                  href: props.href,
-                  label: props.linkLabel,
-                }}
-              />
-            ),
-            divider: (props) => <Divider noIcon={props.noIcon} />,
-            banner: (props) => (
-              <Banner
-                heading={props.heading}
-                bodyText={props.bodyText}
-                externalLink={{
-                  href: props.externalLinkHref,
-                  label: props.externalLinkLabel,
-                }}
-              />
-            ),
-            youtubeEmbed: (props) => (
-              <YouTubeEmbed youtubeLink={props.youtubeLink} />
-            ),
-            tweetEmbed: (props) => <TweetEmbed tweet={props.tweet} />,
-            loopingVideo: (props) => (
-              <LoopingVideo src={props.src} caption={props.caption} />
-            ),
-            image: (props) => (
-              <Image src={props.src} alt={props.alt} caption={props.caption} />
-            ),
-            testimonial: (props) => (
-              <Testimonial
-                quote={props.quote}
-                author={props.author}
-                workplaceOrSocial={props.workplaceOrSocial}
-                socialLink={props.socialLink}
-              />
-            ),
-          }}
-        />
       </div>
     </div>
   );
